3f4925970d05b0da7b89b62f4920daeec385b29d
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-05-06 14:32:02 -0500

    Get the number of words

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -39,6 +39,7 @@ export const Graph = ({ onGetData }) => {
     const chartRef = useRef(null);
     const [chartInstance, setChartInstance] = useState(null);
     const [charCounter, setChartCounter] = useState(0)
+    const [wordCounter, setWordtCounter] = useState(0)
     useEffect(() => {
         if (chartRef && chartRef.current) {
             const newChartInstance = new Chart(chartRef.current, chartConfigBar);
@@ -46,11 +47,17 @@ export const Graph = ({ onGetData }) => {
         }
     }, [chartRef]);
     useEffect(() => {
+        // Data normalization
         const sentence = fixData(onGetData);
         setChartCounter(sentence.length);
+        // Save the repeat words
         const wordMap = countRepeatedWords(sentence);
         delete wordMap[""];
         console.log(wordMap);
+        // Get the number of words
+        const n_words = getNumberWords(wordMap);
+        setWordtCounter(n_words);
+
         if (chartInstance !== null) {
             updateDataset(0, [5, 1, 3]);
         }
@@ -77,12 +84,23 @@ export const Graph = ({ onGetData }) => {
         }
         return wordMap;
     }
+    const getNumberWords = (wordMap) => {
+        const values = Object.values(wordMap)
+        // let words = 0;
+        // wordMap.forEach(item => {
+        //     words += item;
+        // })
+        // return words;
+        const reducer = (acc, cur) => acc + cur;
+        const words = values.reduce(reducer, 0);
+        return words;
+    }
     return (
         <section className="graph">
             <div className="graph__container">
                 <div className="graph__total">
-                    <p>TOTAL WORDS: {charCounter}</p>
-                    <p className="last">TOTAL CHARACTERS:</p>
+                    <p>TOTAL WORDS: {wordCounter}</p>
+                    <p className="last">TOTAL CHARACTERS: {charCounter}</p>
                 </div>
                 <p className="graph__histo-text">HISTOGRAM TOP 3 WORDS</p>
                 <div className="graph__chart">
